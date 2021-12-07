f339816e011b9d31d8b8c3d240a1d302afb6417d
Yeh Lin <linyeah2011@gmail.com>
2021-12-07 18:51:07 +0800

    02- target the element

--- crawler.py
+++ crawler.py
@@ -1,9 +1,21 @@
 from selenium import webdriver
+import time
 CRAWL_URL = 'https://www.apple.com/tw/macbook-pro/'
+TARGET_CLASS_NAME   = 'bottom-intro'
 
 def init_driver():
     driver = webdriver.Chrome('./chromedriver')
     return driver
 
-def crawl(driver):
-    driver.get(CRAWL_URL)
+def crawler_helper(driver):
+    if driver is not None:
+        driver.get(CRAWL_URL)
+    es = driver.find_elements_by_class_name(TARGET_CLASS_NAME)  
+    for e in es:
+        print(e.get_attribute("data-analytics-title"))
+
+def crawl(driver, duration=5):
+    while(True):
+        crawler_helper(driver)
+        time.sleep(duration)
+        
